46e5bbef1b15025729bd4a6c2401e14e8181a73c
ahmadisyraf <isyrafmagic@gmail.com>
2024-10-29 11:31:30 +0800

    update: add spinner

--- app/organization/organization-details.tsx
+++ app/organization/organization-details.tsx
@@ -1,11 +1,11 @@
-import { Box } from "@/components/ui/box";
-import { Button, ButtonText } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
 import { getOrgById } from "@/services/organization";
 import { useState, useEffect } from "react";
 import { useSessionStore } from "@/store/user";
 import CardContainer from "@/components/card-container";
 import { Text } from "react-native";
+import useLoading from "@/hooks/useLoading";
+import { Center } from "@/components/ui/center";
+import { Spinner } from "@/components/ui/spinner";
 
 interface Organization {
   organizationName: string;
@@ -15,19 +15,26 @@ interface Organization {
 export default function OrganizationDetailsScreen() {
   const [orgData, setOrgData] = useState<Organization>();
   const { accessToken } = useSessionStore();
+  const { start, stop, loading } = useLoading();
 
   useEffect(() => {
     const fetchOrg = async () => {
-      try {
-        const org = await getOrgById(accessToken);
-        setOrgData(org.data.organization);
-      } catch (error) {
-        console.error("Error fetching organization:", error);
-      }
+      start();
+      const organization = await getOrgById(accessToken);
+      setOrgData(organization.data.organization);
+      stop();
     };
 
     fetchOrg();
   }, [accessToken]);
+
+  if (loading) {
+    return (
+      <Center>
+        <Spinner size={"large"} />
+      </Center>
+    );
+  }
 
   return (
     <CardContainer
