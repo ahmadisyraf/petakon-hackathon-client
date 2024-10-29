0dd05ff149da713449ccc7ba1738188ca18ad6b6
badzaini <badzaini28@gmail.com>
2024-10-29 11:27:28 +0800

    push code to pull

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,49 @@
 import CreateDonationButton from "@/components/create-donation-button";
 import { Box } from "@/components/ui/box";
 import { Button, ButtonText } from "@/components/ui/button";
+import { useState, useEffect } from "react";
+import { useSessionStore } from "@/store/user";
+import { getDonationStatus } from "@/services/donation";
+
+enum DonationStatusEnum {
+  pending = "pending",
+  completed = "completed",
+  reserved = "reserved",
+}
+
+interface Donation {
+  title: string;
+  description: string;
+  foodType: string;
+  donationSize: string;
+  transportationMethod: string;
+  status: DonationStatusEnum;
+}
 
 export default function HomeScreen() {
+  const [donationPending, setDonationPending] = useState<Donation>();
+  const { accessToken } = useSessionStore();
+
+  useEffect(() => {
+    const fetchDonation = async () => {
+      try {
+        const status = DonationStatusEnum.pending;
+        const donation = await getDonationStatus({ accessToken, status });
+        console.log(donation);
+        setDonationPending(donation.data);
+      } catch (error) {
+        console.error("Error fetching donation:", error);
+      }
+    };
+
+    fetchDonation();
+  }, []);
+
+  console.log(donationPending);
   return (
     <Box>
       <Button>
-        <ButtonText>This is pending order</ButtonText>
+        <ButtonText>{donationPending?.title}</ButtonText>
       </Button>
       <CreateDonationButton />
     </Box>
--- services/donation/index.ts
+++ services/donation/index.ts
@@ -2,6 +2,33 @@ import axios from "axios";
 
 const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
+// enum DonationStatusEnum {
+//   pending = "pending",
+//   completed = "completed",
+//   reserved = "reserved",
+// }
+
+// type DonationStatus = {
+//   status: DonationStatusEnum;
+//   accessToken: string;
+// };
+
+export const getDonationStatus = async ({
+  accessToken,
+  status,
+}: any) => {
+  const getDonation = await axios.get(`${apiUrl}/donation`, {
+    params: {
+      status,
+    },
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return getDonation;
+};
+
 type Donation = {
   title: string;
   description: string;
@@ -12,21 +39,21 @@ type Donation = {
 };
 
 export const createDonation = async ({
-    title,
-    description,
-    foodType,
-    donationSize,
-    transportationMethod,
+  title,
+  description,
+  foodType,
+  donationSize,
+  transportationMethod,
   accessToken,
 }: Donation) => {
   const createdDonation = await axios.post(
     `${apiUrl}/donation`,
     {
-        title,
-        description,
-        foodType,
-        donationSize,
-        transportationMethod,
+      title,
+      description,
+      foodType,
+      donationSize,
+      transportationMethod,
     },
     {
       headers: {
